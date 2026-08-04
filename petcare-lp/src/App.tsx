d067b30620f5730eaa147dae5b7b6f4da9ea5eb3
guilherme_m_alves@estudante.sesisenai.org.br <guilherme_m_alves@estudante.sesisenai.org.br>
2026-08-03 22:12:10 -0300

    inicio de tudo

---
+++ petcare-lp/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline dark:bg-gray-800 dark:text-white">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
