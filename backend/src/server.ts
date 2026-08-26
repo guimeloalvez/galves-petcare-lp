9eb50b1e434e47d527ff3ca5f6517d5cfdb6ab96
Guilherme Melo <guilherme_m_alves@estudante.sesisenai.org.br>
2026-08-25 21:43:48 -0300

    feat: inicializado e configurado API

---
+++ backend/src/server.ts
@@ -0,0 +1,35 @@
+import express, { response, type Request, type Response } from "express"
+import { randomUUID } from "node:crypto"
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get("/health", (_request: Request, response: Response) => {
+    return response.json({
+        status: "ok"
+    })
+})
+
+interface CreateUserBody {
+    name: string;
+}
+
+app.post("/users", (request: Request<object, object, CreateUserBody>, response: Response) => {
+    const name = request.body.name?.trim()
+    if (!name) {
+        return response.status(400).json({
+            error: "Name is required"
+        })
+    }
+
+    return response.status(201).json({
+        id: randomUUID(),
+        name
+    })
+})
+
+app.listen(port, () => {
+    console.log(`API rodando em http://localhost:${port}`);
+})
